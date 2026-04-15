d32d675b72c7a383434924860d45bf41cf825670
SooHyun Kim <taefung2003@gmail.com>
2026-04-15 13:53:34 +0900

    running program

---
+++ src/main/java/com/syu/noticeapi/NoticeapiApplication.java
@@ -0,0 +1,13 @@
+package com.syu.noticeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeapiApplication.class, args);
+	}
+
+}
