0263c7fe3775ee018daf0c1c2a56476109847820
SooHyun Kim <taefung2003@gmail.com>
2026-05-06 16:02:10 +0900

    add EnableScheduling annotation to main

--- src/main/java/com/syu/noticeapi/NoticeapiApplication.java
+++ src/main/java/com/syu/noticeapi/NoticeapiApplication.java
@@ -2,12 +2,13 @@ package com.syu.noticeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NoticeapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoticeapiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeapiApplication.class, args);
+    }
 }
