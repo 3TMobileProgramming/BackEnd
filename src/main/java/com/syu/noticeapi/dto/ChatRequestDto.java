d763c49557dfedc1530f6e120369f34dfe50ae4d
SooHyun Kim <taefung2003@gmail.com>
2026-05-09 21:22:02 +0900

    add dto package

---
+++ src/main/java/com/syu/noticeapi/dto/ChatRequestDto.java
@@ -0,0 +1,16 @@
+package com.syu.noticeapi.dto;
+
+public class ChatRequestDto {
+
+    private String question;
+
+    public ChatRequestDto() {}
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
